c09a371e22f5d6210eb50de50671ef8562a6fdb2
Lucas da Paz Oliveira <xlucaspx@gmail.com>
2025-11-27 08:15:56 -0300

    feat: ajusta TopNexys e constraints

--- proto/Nexys-A7-100T-TP2.xdc
+++ proto/Nexys-A7-100T-TP2.xdc
@@ -74,22 +74,22 @@ set_property -dict {PACKAGE_PIN N15 IOSTANDARD LVCMOS33} [get_ports {o_led_b}]
 # set_property -dict { PACKAGE_PIN U13   IOSTANDARD LVCMOS33 } [get_ports { AN[7] }];  # IO_L23N_T3_A02_D18_14 Sch=an[7]
 
 ##7 segment display
-set_property -dict { PACKAGE_PIN T10   IOSTANDARD LVCMOS33 } [get_ports { SEG[6] }]; #IO_L24N_T3_A00_D16_14 Sch=ca
-set_property -dict { PACKAGE_PIN R10   IOSTANDARD LVCMOS33 } [get_ports { SEG[5] }]; #IO_25_14 Sch=cb
-set_property -dict { PACKAGE_PIN K16   IOSTANDARD LVCMOS33 } [get_ports { SEG[4] }]; #IO_25_15 Sch=cc
-set_property -dict { PACKAGE_PIN K13   IOSTANDARD LVCMOS33 } [get_ports { SEG[3] }]; #IO_L17P_T2_A26_15 Sch=cd
-set_property -dict { PACKAGE_PIN P15   IOSTANDARD LVCMOS33 } [get_ports { SEG[2] }]; #IO_L13P_T2_MRCC_14 Sch=ce
-set_property -dict { PACKAGE_PIN T11   IOSTANDARD LVCMOS33 } [get_ports { SEG[1] }]; #IO_L19P_T3_A10_D26_14 Sch=cf
-set_property -dict { PACKAGE_PIN L18   IOSTANDARD LVCMOS33 } [get_ports { SEG[0] }]; #IO_L4P_T0_D04_14 Sch=cg
+set_property -dict { PACKAGE_PIN T10   IOSTANDARD LVCMOS33 } [get_ports { o_seg[6] }]; #IO_L24N_T3_A00_D16_14 Sch=ca
+set_property -dict { PACKAGE_PIN R10   IOSTANDARD LVCMOS33 } [get_ports { o_seg[5] }]; #IO_25_14 Sch=cb
+set_property -dict { PACKAGE_PIN K16   IOSTANDARD LVCMOS33 } [get_ports { o_seg[4] }]; #IO_25_15 Sch=cc
+set_property -dict { PACKAGE_PIN K13   IOSTANDARD LVCMOS33 } [get_ports { o_seg[3] }]; #IO_L17P_T2_A26_15 Sch=cd
+set_property -dict { PACKAGE_PIN P15   IOSTANDARD LVCMOS33 } [get_ports { o_seg[2] }]; #IO_L13P_T2_MRCC_14 Sch=ce
+set_property -dict { PACKAGE_PIN T11   IOSTANDARD LVCMOS33 } [get_ports { o_seg[1] }]; #IO_L19P_T3_A10_D26_14 Sch=cf
+set_property -dict { PACKAGE_PIN L18   IOSTANDARD LVCMOS33 } [get_ports { o_seg[0] }]; #IO_L4P_T0_D04_14 Sch=cg
 #set_property -dict { PACKAGE_PIN H15   IOSTANDARD LVCMOS33 } [get_ports { DP }];    #IO_L19N_T3_A21_VREF_15 Sch=dp
-set_property -dict { PACKAGE_PIN J17   IOSTANDARD LVCMOS33 } [get_ports { AN[0] }];  #IO_L23P_T3_FOE_B_15 Sch=an[0]
-set_property -dict { PACKAGE_PIN J18   IOSTANDARD LVCMOS33 } [get_ports { AN[1] }];  #IO_L23N_T3_FWE_B_15 Sch=an[1]
-set_property -dict { PACKAGE_PIN T9    IOSTANDARD LVCMOS33 } [get_ports { AN[2] }];  #IO_L24P_T3_A01_D17_14 Sch=an[2]
-set_property -dict { PACKAGE_PIN J14   IOSTANDARD LVCMOS33 } [get_ports { AN[3] }];  #IO_L19P_T3_A22_15 Sch=an[3]
-set_property -dict { PACKAGE_PIN P14   IOSTANDARD LVCMOS33 } [get_ports { AN[4] }];  #IO_L8N_T1_D12_14 Sch=an[4]
-set_property -dict { PACKAGE_PIN T14   IOSTANDARD LVCMOS33 } [get_ports { AN[5] }];  #IO_L14P_T2_SRCC_14 Sch=an[5]
-set_property -dict { PACKAGE_PIN K2    IOSTANDARD LVCMOS33 } [get_ports { AN[6] }];  #IO_L23P_T3_35 Sch=an[6]
-set_property -dict { PACKAGE_PIN U13   IOSTANDARD LVCMOS33 } [get_ports { AN[7] }];  #IO_L23N_T3_A02_D18_14 Sch=an[7]
+set_property -dict { PACKAGE_PIN J17   IOSTANDARD LVCMOS33 } [get_ports { o_an[0] }];  #IO_L23P_T3_FOE_B_15 Sch=an[0]
+set_property -dict { PACKAGE_PIN J18   IOSTANDARD LVCMOS33 } [get_ports { o_an[1] }];  #IO_L23N_T3_FWE_B_15 Sch=an[1]
+set_property -dict { PACKAGE_PIN T9    IOSTANDARD LVCMOS33 } [get_ports { o_an[2] }];  #IO_L24P_T3_A01_D17_14 Sch=an[2]
+set_property -dict { PACKAGE_PIN J14   IOSTANDARD LVCMOS33 } [get_ports { o_an[3] }];  #IO_L19P_T3_A22_15 Sch=an[3]
+set_property -dict { PACKAGE_PIN P14   IOSTANDARD LVCMOS33 } [get_ports { o_an[4] }];  #IO_L8N_T1_D12_14 Sch=an[4]
+set_property -dict { PACKAGE_PIN T14   IOSTANDARD LVCMOS33 } [get_ports { o_an[5] }];  #IO_L14P_T2_SRCC_14 Sch=an[5]
+set_property -dict { PACKAGE_PIN K2    IOSTANDARD LVCMOS33 } [get_ports { o_an[6] }];  #IO_L23P_T3_35 Sch=an[6]
+set_property -dict { PACKAGE_PIN U13   IOSTANDARD LVCMOS33 } [get_ports { o_an[7] }];  #IO_L23N_T3_A02_D18_14 Sch=an[7]
 
 # Buttons
 # set_property -dict { PACKAGE_PIN C12   IOSTANDARD LVCMOS33 } [get_ports { CPU_RESETN }];   # IO_L3P_T0_DQS_AD1P_15 Sch=cpu_resetn
@@ -187,8 +187,8 @@ set_property -dict { PACKAGE_PIN B18   IOSTANDARD LVCMOS33 } [get_ports { i_flam
 # set_property -dict { PACKAGE_PIN C16   IOSTANDARD LVCMOS33 } [get_ports { ACL_INT[2] }]; # IO_L20P_T3_A20_15 Sch=acl_int[2]
 
 ##Temperature Sensor
-set_property -dict { PACKAGE_PIN C14   IOSTANDARD LVCMOS33 } [get_ports { TMP_SCL }]; #IO_L1N_T0_AD0N_15 Sch=tmp_scl
-set_property -dict { PACKAGE_PIN C15   IOSTANDARD LVCMOS33 } [get_ports { TMP_SDA }]; #IO_L12N_T1_MRCC_15 Sch=tmp_sda
+set_property -dict { PACKAGE_PIN C14   IOSTANDARD LVCMOS33 } [get_ports { o_temperature_scl }]; #IO_L1N_T0_AD0N_15 Sch=tmp_scl
+set_property -dict { PACKAGE_PIN C15   IOSTANDARD LVCMOS33 } [get_ports { b_temperature_sda }]; #IO_L12N_T1_MRCC_15 Sch=tmp_sda
 # Temperature Sensor
 # set_property -dict { PACKAGE_PIN C14   IOSTANDARD LVCMOS33 } [get_ports { TMP_SCL }]; # IO_L1N_T0_AD0N_15 Sch=tmp_scl
 # set_property -dict { PACKAGE_PIN C15   IOSTANDARD LVCMOS33 } [get_ports { TMP_SDA }]; # IO_L12N_T1_MRCC_15 Sch=tmp_sda
--- rtl/TopNexysA7.sv
+++ rtl/TopNexysA7.sv
@@ -1,134 +1,150 @@
 `default_nettype none
 
 /**
- * Device: xc7a100tcsg324-1
+ * Módulo que programa a placa Nexys A7, fazendo a integração entre sensores e o mapeamento dos pinos, switches e botões
+ * utilizados. Dispositivo: `xc7a100tcsg324-1`.
  *
- * i_clock: Sinal de clock da placa;
- * i_reset: Sinal de reset mapeado para o botão `n17`;
- * o_leds: Vetor de bits no qual cada posição representa um led da placa.
+ * [Wires]
+ * - i_clock:             Sinal de clock da placa.
+ * - i_reset:             Sinal de reset mapeado para o botão `N17`.
+ * - i_simulation_switch: `1` para ativar a simulação, mapeado para o switch `V10`.
+ * - i_fire_on:           `1` para aumentar a temperatura quando a simulação está ativa, mapeado para o switch `U11`.
+ * - i_co2_sensor_data:   Dados do sensor de CO2, mapeado para o pino `AD10N`.
+ * - i_flame_sensor:      Dados do sensor de chama, mapeado para o pino `AD11P`.
+ * - b_temperature_sda:   I2C SDA (bidirecional) no sensor de temperatura.
+ * - o_temperature_scl:   I2C SCL no sensor de temperatura.
+ * - o_seg:               7 segmentos de cada display.
+ * - o_an:                8 ânodos de 8 displays.
+ * - o_leds:              Vetor de bits no qual cada posição representa um led da placa.
+ * - o_led_r:             Led vermelho.
+ * - o_led_g:             Led verde.
+ * - o_led_b:             Led azul.
+ * - o_pin_sound:         `1` para ativar o buzzer, mapeado para o pino `AD3P`.
  */
 module TopNexysA7(
 	input var logic i_clock,
-	input var logic i_reset, 			// N17 (Mid Button)
-	input var logic i_simulation_switch,// V10 Switch
-	input var logic i_fire_on, 			// U11 Switch
-	input var logic i_co2_sensor_data, 	// Pin AD10N
-	input var logic i_flame_sensor,		// Pin AD11P
+	input var logic i_reset,
+	input var logic i_simulation_switch,
+	input var logic i_fire_on,
+	input var logic i_co2_sensor_data,
+	input var logic i_flame_sensor,
 
-	inout tri        TMP_SDA,          			// i2c sda on temp sensor - bidirectional
+	inout tri b_temperature_sda,
 
-    output var logic       TMP_SCL,          	// i2c scl on temp sensor
-	output var logic [6:0]  SEG,              	// 7 segments of each display
-    output var logic [7:0]  AN,              	// 8 anodes of 8 displays
+	output var logic o_temperature_scl,
+	output var logic [6:0] o_seg,
+	output var logic [7:0] o_an,
 	output var logic [15:0] o_leds,
-	output var logic o_led_r,				 	// Red led rgb
-	output var logic o_led_g,				 	// Green led rgb
-	output var logic o_led_b,				 	// Blue led rgb
-	output var logic o_pin_sound 				// Pin AD3P
+	output var logic o_led_r,
+	output var logic o_led_g,
+	output var logic o_led_b,
+	output var logic o_pin_sound
 );
 
-logic sound;
-logic buzzer_sound;
-SoundBuzzer u_soundBuzzer (
-	.i_clock(i_clock),
-	.i_reset(i_reset),
-	.i_sound(sound),
-	.o_pin_sound(buzzer_sound)
-);
+	logic sound;
+	logic buzzer_sound;
 
-TemperatureHumidity u_temperatureHumidity(.i_clock(i_clock));
+	logic fire_signal;
 
-logic fire_signal;
-FireController #(.SECONDS(7)) u_fireController (
-	.i_clock(i_clock),
-	.i_reset(i_reset),
-	.i_flame_sensor(i_flame_sensor),
-	.o_fire(fire_signal),
-	.costumer(u_temperatureHumidity)
-);
+	logic co2_signal;
 
-logic co2_signal;
-Co2Sensor u_Co2Sensor (
-	.i_clock(i_clock),
-	.i_reset(i_reset),
-	.i_sensor_data(i_co2_sensor_data),
-	.o_gas_detected(co2_signal)
-);
+	logic system_clock_200khz;
+	logic [7:0] temperature_celsius;
+	logic [11:0] temperature_celsius_extended;
 
-wire w_200KHz;                  // 200kHz SCL
-wire [7:0] c_data;              // 8 bits of Celsius temperature data
-wire [7:0] f_data;              // 8 bits of Fahrenheit temperature data
-wire [11:0] c_extended_data;
+	TemperatureHumidity u_temperature_humidity( .i_clock(i_clock) );
 
-// Instantiate i2c master
-I2cMaster i2cmaster(
-	.clk_200KHz(w_200KHz),
-	.temp_data(c_data),
-	.temp_data_full_precision(c_extended_data),
-	.SDA(TMP_SDA),
-	.SCL(TMP_SCL)
-);
+	Clkgen_200KHz u_clock_generator (
+		.clk_100MHz(i_clock),
+		.clk_200KHz(system_clock_200khz)
+	);
 
-// Instantiate 200kHz clock generator
-Clkgen_200KHz clkgen(
-	.clk_100MHz(i_clock),
-	.clk_200KHz(w_200KHz)
-);
+	I2cMaster u_i2c_master (
+		.clk_200KHz(system_clock_200khz),
+		.temp_data(temperature_celsius),
+		.temp_data_full_precision(temperature_celsius_extended),
+		.SDA(b_temperature_sda),
+		.SCL(o_temperature_scl)
+	);
 
+	Seg7c u_seg7_control (
+		.clk_100MHz(i_clock),
+		.c_data((u_temperature_metrics.data_array[9:0])),
+		.SEG(o_seg),
+		.AN(o_an)
+	);
 
-TemperatureMetrics #(.SECONDS(3)) u_temperatureMetrics (
-	.i_clock(i_clock),
-	.i_reset(i_reset),
-	.i_simulation(i_simulation_switch),
-	.i_fire_on(i_fire_on),
-	.i_temperature(c_extended_data),
-	.provider(u_temperatureHumidity)
-);
+	TemperatureMetrics #(.SECONDS(3)) u_temperature_metrics (
+		.i_clock(i_clock),
+		.i_reset(i_reset),
+		.i_simulation(i_simulation_switch),
+		.i_fire_on(i_fire_on),
+		.i_temperature(temperature_celsius_extended),
+		.provider(u_temperature_humidity)
+	);
 
-Seg7c segcontrol(
-	.clk_100MHz(i_clock),
-	.c_data((u_temperatureMetrics.data_array[9:0])), // 10 bits Celsius temperature data
-	.SEG(SEG),
-	.AN(AN)
-);
+	FireController #(.Seconds(7)) u_fireController (
+		.i_clock(i_clock),
+		.i_reset(i_reset),
+		.i_flame_sensor(i_flame_sensor),
+		.o_fire(fire_signal),
+		.costumer(u_temperature_humidity)
+	);
 
-logic [23:0] counter_leds;
-always_ff @(posedge i_clock, posedge i_reset) begin
-	if (i_reset) begin
-		o_leds <= 4'h0000;
-		sound <= 0;
-	end else begin
-		o_leds [0] <= fire_signal ? 1'b1 : 1'b0;
-		o_leds [1] <= i_flame_sensor ? 1'b1 : 1'b0;
-		o_leds [2] <= sound ? 1'b1 : 1'b0;
-		o_leds [15] <= i_simulation_switch;
-		o_leds [14] <= i_fire_on;
-		sound <= co2_signal;
+	Co2Sensor u_Co2Sensor (
+		.i_clock(i_clock),
+		.i_reset(i_reset),
+		.i_sensor_data(i_co2_sensor_data),
+		.o_gas_detected(co2_signal)
+	);
 
-		// led RGB only shows when it is simulating
-		if (i_simulation_switch && fire_signal) begin
-			counter_leds <= counter_leds + 1;
-			if (counter_leds[22] == 1'b0) begin
-				o_led_r <= 1;
-				o_led_g <= 0;
-				o_led_b <= 0;
-			end else if (counter_leds[23] == 1'b0) begin
-				o_led_r <= 0;
-				o_led_g <= 1;
-				o_led_b <= 0;
+	SoundBuzzer u_soundBuzzer (
+		.i_clock(i_clock),
+		.i_reset(i_reset),
+		.i_sound(sound),
+		.o_pin_sound(buzzer_sound)
+	);
+
+	logic [23:0] counter_leds;
+
+	always_ff @(posedge i_clock, posedge i_reset) begin
+		if (i_reset) begin
+			o_leds <= 4'h0000;
+			sound <= 0;
+		end else begin
+			o_leds [0] <= fire_signal;
+			o_leds [1] <= i_flame_sensor;
+			o_leds [2] <= sound;
+			o_leds [15] <= i_simulation_switch;
+			o_leds [14] <= i_fire_on;
+			sound <= co2_signal;
+
+			// led RGB only shows when it is simulating
+			if (i_simulation_switch && fire_signal) begin
+				counter_leds <= counter_leds + 1;
+				if (counter_leds[22] == 1'b0) begin
+					o_led_r <= 1;
+					o_led_g <= 0;
+					o_led_b <= 0;
+				end
+				else if (counter_leds[23] == 1'b0) begin
+					o_led_r <= 0;
+					o_led_g <= 1;
+					o_led_b <= 0;
+				end
+				else begin
+					o_led_r <= 0;
+					o_led_g <= 0;
+					o_led_b <= 1;
+				end
 			end
 			else begin
 				o_led_r <= 0;
 				o_led_g <= 0;
-				o_led_b <= 1;
+				o_led_b <= 0;
 			end
-		end else begin
-			o_led_r <= 0;
-			o_led_g <= 0;
-			o_led_b <= 0;
+			o_pin_sound <= buzzer_sound;
 		end
-		o_pin_sound <= buzzer_sound;
 	end
-end
 
 endmodule: TopNexysA7
